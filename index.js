d50aa637ae6b9389fb1c728d985e8a3ef45ba2c9
Abdulrahman <oye1965@gmail.com>
2022-11-02 15:35:19 +0100

    javascrpit

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ students = 'sade'
 console.log(students)
 console.log(fruit)
 
+let num1 = 23
+let test = false
+
+console.log(typeof(test))
+console.log(typeof(num1).toString)
+
+console.log(1+3*3)
+
+console.log(`abcd`)
+ 
+
+  if(num1 == test){
+    console.log('num1 is normal')
+  }else{
+    console.log('num1 is abnormal')
+  }
