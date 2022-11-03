72c9083249a98cac96a63069cb7ce194d1687556
Abdulrahman <oye1965@gmail.com>
2022-11-03 13:52:16 +0100

    added js

--- index.js
+++ index.js
@@ -21,3 +21,39 @@ console.log(`abcd`)
   }else{
     console.log('num1 is abnormal')
   }
+
+
+  let age = 20
+  if(age >18){
+    console.log('permission is granted')
+  }else if(age >= 18){
+    console.log('permission granted')
+  } else{
+    console.log('permission denied')
+  }
+    
+
+  let num3 = 100.009
+  console.log(Math.floor(num3))
+  console.log(Math.trunc(num3))
+
+
+  let firstName = 'Abdulrahman Oloyede'
+
+  console.log(firstName.length)
+  console.log(firstName.toUpperCase())
+  console.log(firstName.toLowerCase())
+
+
+   let arr = [10,2,3,4,5,6,7,8,9]
+  // console.log(arr.length)
+  // console.log(arr.push(56))
+
+
+
+//for(begin,condition,step){
+//}
+
+for(let i = 0; i < arr.length; i+= 2){
+        console.log(arr[i] )
+}
